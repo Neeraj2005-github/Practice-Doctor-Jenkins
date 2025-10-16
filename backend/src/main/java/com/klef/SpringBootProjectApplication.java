5dbac4e316bdd4b638457227b0ae881d06133a04
Neeraj <sakalaneeraj1974@gmail.com>
2025-10-16 10:27:21 +0530

    updated

---
+++ backend/src/main/java/com/klef/SpringBootProjectApplication.java
@@ -0,0 +1,17 @@
+package com.klef;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootProjectApplication extends SpringBootServletInitializer
+{
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectApplication.class, args);
+		System.out.println("Project is Running ...");
+	}
+	
+}
